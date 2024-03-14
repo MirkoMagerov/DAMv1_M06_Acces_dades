582f1ad41d9885838018f663a40765e6fe688577
Mirko <miroslav.magerov.7e5@itb.cat>
2024-03-14 13:54:45 +0100

    EA2 a la mitad

---
+++ cat.itb.M6UF1EA2/cat.itb.M6UF1EA2/Categories.cs
@@ -0,0 +1,19 @@
+﻿namespace cat.itb.M6UF1EA2
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Categories(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"CATEGORY (ID: {Id} | Name: {Name})";
+        }
+    }
+}
---
+++ cat.itb.M6UF1EA2/cat.itb.M6UF1EA2/Product2.cs
@@ -0,0 +1,25 @@
+﻿namespace cat.itb.M6UF1EA2
+{
+    public class Product2
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public string Picture { get; set; }
+        public List<Categories> Categories { get; set; }
+
+        public Product2(string name, double price, int stock, string picture, List<Categories> categories)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+            Picture = picture;
+            Categories = categories;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: {Price}, Stock: {Stock}, Picture: {Picture}, Categories: {string.Join(", ", Categories)}";
+        }
+    }
+}
